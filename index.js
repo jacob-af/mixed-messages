5ca2cd3f7137e8bed01895d38ee1e1141774783e
Jacob Feitler <jfeitler@gmail.com>
2020-11-28 14:40:21 -0500

    all seems to be working

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-let cheerfulMessage = ["No matter how bad it gets, remember that you will eventually die",
- "don't worry about your mistakes, life is meaningless anyway",
-"If you're feeling lonely, you can always hold your own hand"]
+let msg1 = ["No matter how bad it gets, ",
+ "Don't worry about your mistakes, ",
+"If you're feeling lonely, "]
+
+let msg2 = ["you will die some day. ", "no one is paying attention. ", "remember that existence is meaningless. "]
+
+let msg3 = ["Eat more ice cream!", "Treat yo'self!", "Life is suffering!"]
 
 let buttn = document.getElementById("try-it");
 
 buttn.onclick = () => {
-    document.getElementById('message').innerHTML = cheerfulMessage[Math.floor(Math.random()*3)]
+    document.getElementById('message').innerHTML = msg1[Math.floor(Math.random()*3)]+ msg2[Math.floor(Math.random()*3)]+msg3[Math.floor(Math.random()*3)]
 }
